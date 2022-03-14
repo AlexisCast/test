2ec2e4f6bb91d1e600a3957cd9a62f2aa926c0f6
Alexis Castellanos <castellanos.alexis@hotmail.com>
2022-03-14 14:12:58 -0600

    added stuffd

--- test.js
+++ test.js
@@ -1,2 +1,7 @@
 console.log('Hello');
+console.log('Added line');console.log('Hello');
+console.log('Added line');console.log('Hello');
+console.log('Added line');console.log('Hello');
+console.log('Added line');console.log('Hello');
+console.log('Added line');console.log('Hello');
 console.log('Added line');
--- test3.js
+++ test3.js
@@ -1,3 +1,6 @@
 console.log('Hello');
 console.log('Added line');console.log('Hello');
+console.log('Added line');console.log('Hello');
+console.log('Added line');console.log('Hello');
+console.log('Added line');console.log('Hello');
 console.log('Added line');
